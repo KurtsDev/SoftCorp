4328796c5d3f1df1307bb52c2cad77013136a28f
Kurts <kurts.dev@gmail.com>
2020-02-16 13:36:23 +0300

    read json

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>softcorp</title>
+    <script src="script.js"></script>
+    <style>
+
+    </style>
+</head>
+<body>
+
+</body>
+</html>
