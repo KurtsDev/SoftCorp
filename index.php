53dfb18c8e532954bd1207011b1de4b9663556fc
Kurts <kurts.dev@gmail.com>
2020-02-20 16:36:28 +0300

    buy

--- index.php
+++ index.php
@@ -13,22 +13,10 @@ header('Access-Control-Allow-Origin: *');
     </style>
 </head>
 <body>
-<!---->
-<!--<div id="category">-->
-<!--    <div class="name-category">Книги</div>-->
-<!--    <div class="item">-->
-<!--        <div class="name">Алгоритмы. Построение и анализ. Т. Кормен, Ч. Лейзерсон, Р. Ривест, К. Штайн.</div>-->
-<!--        <div class="price">12</div>-->
-<!--    </div>-->
-<!--</div>-->
-<!---->
-<!--<div id="myDynamicTable">-->
-<!---->
-<!---->
-<!---->
-<!--</div>-->
 
+<div id="cartwrap">
 
+</div>
 
 </body>
 <script src="script.js"></script>
--- script.js
+++ script.js
@@ -7,131 +7,193 @@ function getFile(fileName) {
 
 let data = getFile('task/data.json');
 let names = getFile('task/names.json');
-
 let goods = data['Value']['Goods'];
 
-let productPropertiesArr = [];
+let structureObj = createStructureObj(goods, names);
 
-for (let [key, value] of Object.entries(goods)) {
-    let productPropertiesId = value['T'];
-    productPropertiesArr[productPropertiesId] = value
-
-}
-
-let productNamesArr = [];
-
-for (let [key, value] of Object.entries(names)) {
-    let product = value.B;
-    let group = value.G;
-    for (let [key, value] of Object.entries(product)) {
-        value.group = group;
-        productNamesArr[key] = value;
+function createStructureObj(objectA, objectB) {
+    let productPropertiesArr = [];
+    for (let [key, value] of Object.entries(objectA)) {
+        let productPropertiesId = value['T'];
+        productPropertiesArr[productPropertiesId] = value
     }
-}
 
-for (let [key, value] of Object.entries(productPropertiesArr)) {
-    for (let [key1, value1] of Object.entries(productNamesArr)) {
-        if (key === key1) {
-            Object.assign(value, value1);
+    let productNamesArr = [];
+    for (let [key, value] of Object.entries(objectB)) {
+        let product = value.B;
+        let group = value.G;
+        for (let [key, value] of Object.entries(product)) {
+            value.group = group;
+            productNamesArr[key] = value;
         }
-
     }
+
+    for (let [key, value] of Object.entries(productPropertiesArr)) {
+        for (let [key1, value1] of Object.entries(productNamesArr)) {
+            if (key === key1) {
+                Object.assign(value, value1);
+            }
+        }
+    }
+
+    let structureObj = [];
+    for (let [key, value] of Object.entries(productPropertiesArr)) {
+        let group = value.group;
+        if (!structureObj[group]) {
+            structureObj[group] = [];
+        }
+        structureObj[group].push(value);
+    }
+
+    return structureObj;
 
 }
 
 
-structureArr = [];
+console.log(structureObj);
 
-for (let [key, value] of Object.entries(productPropertiesArr)) {
-    let group = value.group;
-    if (!structureArr[group]) {
-        structureArr[group] = [];
+drawing(structureObj);
+
+function drawing(obj) {
+    for (let [key, value] of Object.entries(obj)) {
+
+        let divCategory = document.createElement('div',);
+        divCategory.id = 'category';
+        document.body.append(divCategory);
+
+        let nameCategory = document.createElement('div',);
+        nameCategory.className = 'name-category';
+        divCategory.append(nameCategory);
+        nameCategory.appendChild(document.createTextNode(key));
+
+        for (let i = 0; i < value.length; i++) {
+
+            let divItem = document.createElement('div',);
+            divItem.className = 'item';
+            divItem.setAttribute('category', key);
+            divItem.setAttribute('productName', value[i].N);
+            divItem.setAttribute('productPrice', value[i].C);
+            divItem.setAttribute('left', value[i].P);
+            divItem.addEventListener('click', buy);
+            nameCategory.append(divItem);
+
+            let divName = document.createElement('div',);
+            divName.className = 'name';
+            divItem.append(divName);
+            divName.appendChild(document.createTextNode(value[i].N + ' '));
+            divName.appendChild(document.createTextNode('(' + value[i].P + ')'));
+
+            let divPrice = document.createElement('div',);
+            divPrice.className = 'price';
+            divItem.append(divPrice);
+
+            divPrice.appendChild(document.createTextNode(convertUSD(value[i].C)));
+        }
     }
-
-    structureArr[group].push(value);
 }
-console.log(structureArr);
 
-for (let [key, value] of Object.entries(structureArr)) {
-
-
-    let divCategory = document.createElement('div',);
-    divCategory.id = 'category';
-    document.body.append(divCategory);
-
-
-    let nameCategory = document.createElement('div',);
-    nameCategory.className = 'name-category';
-    divCategory.append(nameCategory);
-    nameCategory.appendChild(document.createTextNode(key));
-
-    for (let i = 0; i < value.length; i++) {
-
-    let divItem = document.createElement('div',);
-    divItem.className = 'item';
-    nameCategory.append(divItem);
-
-
-
-        let divName = document.createElement('div',);
-        divName.className = 'name';
-        divItem.append(divName);
-        divName.appendChild(document.createTextNode(value[i].N));
-
-
-
-        let divPrice = document.createElement('div',);
-        divPrice.className = 'price';
-        divItem.append(divPrice);
-
-        divPrice.appendChild(document.createTextNode(value[i].C));
-    }
-
-
+function convertUSD(value) {
+    let usd = 2.1;
+    value = value * 1000 * usd / 1000;
+    return value;
 }
 
 
-// function addTable(structureArr) {
-//     var myTableDiv = document.getElementById("myDynamicTable");
-//     var table = document.createElement('TABLE');
-//     //table.border = '1';
-//     var tableBody = document.createElement('TBODY');
-//     table.appendChild(tableBody);
-//
+let buyList = [];
+
+function buy(e) {
+    let category = e.currentTarget.getAttribute('category');
+    let productName = e.currentTarget.getAttribute('productName');
+    let productPrice = e.currentTarget.getAttribute('productPrice');
+    let left = e.currentTarget.getAttribute('left');
+    let quantity = 0;
+
+    if (!buyList[category]) {
+        buyList[category] = [];
+    }
+
+    let productObj =  {
+        N: productName,
+        C: productPrice,
+        P: left,
+        Q: quantity,
+    };
+
+    buyList2 = [];
 
 
-// for (let [key, value] of Object.entries(productPropertiesArr)) {
-//
-//     let prod = value;
-//
-//     var th = document.createElement('TH');
-//     th.appendChild(document.createTextNode(key));
-//     var tr = document.createElement('TR');
-//
-//
-//
-//
-//     tableBody.appendChild(tr);
-//     for (let [key, value] of Object.entries(prod)) {
-//         var td = document.createElement('TD');
-//         //td.width = '75';
-//         td.appendChild(document.createTextNode(  value));
-//         tr.appendChild(td);
-//     }
-//
-//
-//
-//
-// }//
+
+    for (let [key, value] of Object.entries(buyList)) {
+
+        value.
 
 
-//
-//
-//     myTableDiv.appendChild(table);
-// }
-// addTable()
-//
-//
+    }
+
+
+
+
+    buyList[category].push(productObj);
+
+    drawingCart(buyList);
+
+
+}
+
+
+
+function drawingCart(obj) {
+    let divCategoryWrap = document.getElementById('cartwrap');
+    divCategoryWrap.innerHTML = '';
+    for (let [key, value] of Object.entries(obj)) {
+
+
+
+
+
+        let divCategory = document.createElement('div',);
+        divCategory.className = 'category';
+        divCategoryWrap.append(divCategory);
+
+        let nameCategory = document.createElement('div',);
+        nameCategory.className = 'name-category';
+        divCategory.append(nameCategory);
+        nameCategory.appendChild(document.createTextNode(key));
+
+        for (let i = 0; i < value.length; i++) {
+
+            let divItem = document.createElement('div',);
+            divItem.className = 'item';
+            divItem.setAttribute('category', key);
+            divItem.setAttribute('productName', value[i].N);
+            divItem.setAttribute('productPrice', value[i].C);
+            divItem.setAttribute('left', value[i].P);
+            divItem.addEventListener('click', buy);
+            nameCategory.append(divItem);
+
+            let divName = document.createElement('div',);
+            divName.className = 'name';
+            divItem.append(divName);
+            divName.appendChild(document.createTextNode(value[i].N + ' '));
+            divName.appendChild(document.createTextNode('(' + value[i].P + ')'));
+
+
+            let divQuantity = document.createElement('div',);
+            divQuantity.className = 'quantity';
+            divItem.append(divQuantity);
+            divQuantity.appendChild(document.createTextNode( value[i].Q ));
+
+
+
+            let divPrice = document.createElement('div',);
+            divPrice.className = 'price';
+            divItem.append(divPrice);
+
+            divPrice.appendChild(document.createTextNode(convertUSD(value[i].C)));
+        }
+    }
+}
+
 
 
 
